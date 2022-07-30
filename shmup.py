82e2c234dfab39025da11bd2487d5346325cec18
ratchek <git@ratchek.com>
2022-07-30 12:23:10 -0400

    Implement mob generation and movement

--- shmup.py
+++ shmup.py
@@ -3,9 +3,9 @@ import pygame
 import random
 import os
 
-WIDTH = 360
-HEIGHT = 480
-FPS = 30
+WIDTH = 400 
+HEIGHT = 600
+FPS = 60
 
 # define colors
 WHITE = (255, 255, 255)
@@ -21,9 +21,31 @@ screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("My Game")
 clock = pygame.time.Clock()
 all_sprites = pygame.sprite.Group()
+mobs = pygame.sprite.Group()
 folder = os.path.abspath(os.path.dirname(__file__))
 images_folder = os.path.join(folder, "resources/img/")
 player_image = pygame.image.load(os.path.join(images_folder, "playerShip1_blue.png")).convert_alpha()
+
+class Mob (pygame.sprite.Sprite):
+    def __init__(self, width, height):
+        super().__init__()
+        self.image = pygame.Surface((width, height))
+        self.width = width
+        self.height = height
+        color = random.choice([WHITE,BLACK,RED,BLUE])
+        self.image.fill(color)
+        self.rect = self.image.get_rect()
+        self.spawn_at_top()
+    def spawn_at_top(self):
+        self.rect.left = random.randrange(WIDTH-self.width)
+        self.rect.bottom = random.randrange(-100, -10)
+        self.speedy = random.randrange(1,8)
+        self.speedx = random.randrange(-3,3)
+    def update(self):
+        self.rect.x += self.speedx
+        self.rect.y += self.speedy
+        if self.rect.top >= HEIGHT or self.rect.right < 0 or self.rect.right>WIDTH:
+            self.spawn_at_top()
 
 class Player(pygame.sprite.Sprite):
     def __init__(self):
@@ -31,15 +53,27 @@ class Player(pygame.sprite.Sprite):
         
         self.image =player_image
         self.rect = self.image.get_rect()
-        self.rect.center =  (WIDTH/2, HEIGHT/2)
-        
+        self.rect.center =  (WIDTH/2, HEIGHT-60)
+        self.speedx = 0
     def update(self):
-        self.rect.x +=1
-        self.rect.y +=1
+        self.speedx = 0
+        keystate = pygame.key.get_pressed()
+        if keystate[pygame.K_LEFT]:
+            self.speedx -= 8
+        if keystate[pygame.K_RIGHT]:
+            self.speedx += 8
+        self.rect.x += self.speedx
+        if self.rect.left < 0:
+            self.rect.left = 0
+        if self.rect.right > WIDTH:
+            self.rect.right = WIDTH
 
 player = Player()
 all_sprites.add(player)
-
+for x in range(10):
+    mob = Mob(30,40)
+    all_sprites.add(mob)
+    mobs.add(mob)
 # Game loop
 running = True
 while running:
@@ -54,7 +88,6 @@ while running:
             running = False
     # Update
     all_sprites.update()
-
     # Draw / render
     screen.fill(GREEN)
     all_sprites.draw(screen)
